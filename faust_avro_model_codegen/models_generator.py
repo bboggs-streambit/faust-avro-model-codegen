fb85df36bf27cf4e504f722533fd37232d09c8cf
brad.boggs <brad.boggs@aggrowth.com>
2024-04-05 15:38:42 -0500

    expose additional functionality to library users

--- faust_avro_model_codegen/models_generator.py
+++ faust_avro_model_codegen/models_generator.py
@@ -8,6 +8,7 @@ from plumbum.cmd import autoflake, black  # type: ignore
 
 from .schema_verifier import SchemaVerifier
 from .template_renderer import TemplateRenderer, RenderedTemplate
+from .template_writer import TemplateWriter
 from .types import (
     CodeGenResultData,
     SchemaData,
@@ -20,9 +21,11 @@ class FaustAvroModelGen:
         self,
         renderer: TemplateRenderer,
         verifier: SchemaVerifier,
+        writer: TemplateWriter,
     ):
         self.renderer = renderer
         self.verifier = verifier
+        self.writer = writer
 
     def generate_module(
         self, schemas: typing.Iterable[SchemaData], outfile: pathlib.Path
@@ -37,30 +40,8 @@ class FaustAvroModelGen:
         )
         self.write(outfile, self.renderer.render(accum_codegen_data))
 
-    @classmethod
-    def write(cls, filepath: pathlib.Path, rendered_text: RenderedTemplate) -> None:
-        if not filepath.parent.exists():
-            filepath.parent.mkdir(parents=True)
-
-        filepath.write_text(rendered_text)
-
-        cls._post_process_output_file(filepath)
-
-    @staticmethod
-    def _post_process_output_file(file: pathlib.Path) -> None:
-        isort.file(
-            file,
-            overwrite_in_place=True,
-            remove_redundant_aliases=True,
-            include_trailing_comma=True,
-            group_by_package=True,
-            float_to_top=True,
-            dedup_headings=True,
-            force_sort_within_sections=True,
-            quiet=True,
-        )
-        autoflake(file.absolute(), "--in-place", "--remove-all-unused-imports")
-        black(file.absolute())
+    def write(self, outfile: pathlib.Path, rendered_text: RenderedTemplate) -> None:
+        self.writer.write(outfile, rendered_text)
 
     def verify_schemas(
         self, schemas: typing.Iterable[SchemaData], module_name: str
